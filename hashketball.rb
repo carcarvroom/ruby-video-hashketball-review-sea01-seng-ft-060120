236f5a6ca70afe0a979e4edf55013a03540bb3b1
Carly L <carlylynnla@gmail.com>
2020-06-01 21:41:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,14 +191,14 @@ end
 
 
 def big_shoe_rebounds
-  largest_shoe_size = 0
+largest_shoe_size = 0
 game_hash.each do |teams, info|
-  players_stats = info[:players]
-  players_stats.each do |player|
-    if player[:shoe] > largest_shoe_size
-      largest_shoe_size = player[:shoe]
+    players_stats = info[:players]
+    players_stats.each do |player|
+      if player[:shoe] > largest_shoe_size
+        largest_shoe_size = player[:shoe]
+      end
     end
-  end 
   players_stats.each do |player|
     if player[:shoe] == largest_shoe_size
      return player[:rebounds]
