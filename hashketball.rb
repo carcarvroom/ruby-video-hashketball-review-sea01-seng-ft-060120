19ec847859cd18ad676fcfb2eebd92ce407af4e4
Carly L <carlylynnla@gmail.com>
2020-06-01 21:36:36 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,8 +168,8 @@ end
 def player_numbers(teamname)
   jersey_numbers = []
   game_hash.each do |teams, info|
+    player_stats = info[:player]
     if info[:team_name] == teamname
-      player_stats = info[:player]
       player_stats.each do |player|
         jersey_numbers << player[:number]
       end
