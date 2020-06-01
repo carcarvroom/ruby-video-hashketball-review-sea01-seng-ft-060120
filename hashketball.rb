07621c76c4ea738924e4f123a0adf46d8759aa6e
Carly L <carlylynnla@gmail.com>
2020-06-01 21:08:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,7 @@ end
 def num_points_scored(playername)
   playname.each do |team, team_info|
     team_info.each do |keys, specifics|
-      if specifics.has== playername
+      if specifics.value?(playername)
         return keys[:points]
       end
     end
